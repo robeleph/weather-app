c856999aa4caff7ef79aff1978b55c2426c2a116
robel_ephraim <ephraimrobel15@gmail.com>
2022-04-29 16:53:11 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search...">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data () {
+    return {
+      
+    }
+  }
+ 
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montserrat', sans-serif;
+}
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+}
+</style>
